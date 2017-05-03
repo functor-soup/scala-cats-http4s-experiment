8024e0e9c147472ad3b89e86b5eccdb3192adeca
luke <nerorevenge@gmail.com>
2017-05-03 08:49:42 -0400

    Working state  - code has to be refactored   - have to use a monad transformer, as I cannot get the value of Stype   String inside Task

--- src/main/scala/com/example/Hello.scala
+++ src/main/scala/com/example/Hello.scala
@@ -9,18 +9,32 @@ import org.http4s.util.StreamApp
 import org.http4s.client.blaze._
 import cats.implicits._
 import constants.Constants
+import cats.syntax.applicative._
+import cats.data.Writer
+import org.http4s.client.Client
 
 object Hello extends StreamApp {
 
+  type Logger[A] = Writer[String,A]
+
   val httpClient = PooledHttp1Client()
 
+  def kik(url: String):Task[String] = {
+    val egg = "[LOG] client issued a get request\n".tell
+      .map(_ => httpClient.expect[String](Uri.unsafeFromString(url)))
+      .flatMap(x => for { _ <- s"[LOG] response recieved a $x \n".tell} yield x)
+
+   val (log, reg) = egg.run
+   println(log)
+   return reg
+  }
+
   val service = HttpService {
-    case GET -> Root => httpClient.expect[String](Uri.unsafeFromString(Constants.mainUrl))
-                                  .flatMap(Ok(_))
+    case GET -> Root => kik(Constants.mainUrl).flatMap(Ok(_))
    }
 
   override def stream(args: List[String]): Stream[Task, Nothing] = {
-  BlazeBuilder
+    BlazeBuilder
       .bindHttp(8080, "localhost")
       .mountService(service, "/")
       .serve
